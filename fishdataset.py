54c4946b03328be1bef2f008e6c7295b722fa6eb
Alrik Firl <afirlortwo@gmail.com>
2018-06-12 00:54:59 -0700

    adding detection proposals in, fixing up the dataset data loader

--- fishdataset.py
+++ fishdataset.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 import torch
 from torch.utils.data import DataLoader
@@ -7,27 +8,43 @@ import cv2
 import utils
 
 class FishData(torch.utils.data.DataLoader):
-    def __init__(self, seqlist_path, data_dir, mode, transform=None, num_epochs=1):
+    def __init__(self, seqlist_path, data_dir, mode, transform=None, num_epochs=1, det_prop_dir=None):
         if not os.path.exists(data_dir):
             print('Error: data path {} does not exist'.format(data_dir))
+
+        self.detprop_basedir = det_prop_dir
+        self.prop_TH = 0.9
 
         self.data_sequences = utils.read_fish_dataset(seqlist_path, data_dir)
         self.seqkeys = list(self.data_sequences.keys())
         self.num_epochs = num_epochs
         self.mode = mode
+
         self.seq_idx = 0
         self.epoch_idx = 0
+        self.detection_proposals = None
+        self.transform = transform
+
         self.load_sequence()
 
     def load_sequence(self):
         seq = self.data_sequences[self.sequence_key()]
-        self.seq_images = seq['frames']
-        self.seq_detection = seq['detections']
+
+        seq_detprop_dir = os.path.join(self.detprop_basedir, self.sequence_key())
+        if os.path.isdir(seq_detprop_dir):
+            det_fnames = sorted(glob.glob(os.path.join(seq_detprop_dir, '*.pkl')), key=utils.natural_sort)
+            det_proposals = utils.read_fish_proposals(det_fnames)
+            self.detection_proposals = utils.merge_fish_proposals(det_proposals)
+        else:
+            self.detection_proposals = None
+
+        self.seq_images = [s['frame'] for s in seq]
+        self.seq_detection = [s['detections'] for s in seq]
         #TODO: do we want anything else? e.g. to have which frames have detections?
 
         #TODO: do we want to hav this be arranged by instance? i.e. we could just skip all the
         #frames inbetween detections
-        self.seq_dets = [det['detections'] for det in self.seq_detection if det['detections'] if not None]
+        self.seq_dets = [det for det in self.seq_detection if det if not None]
 
     def next_sequence(self):
         self.seq_idx += 1
@@ -50,10 +67,15 @@ class FishData(torch.utils.data.DataLoader):
 
     def __getitem__(self, index):
         img_path = self.seq_images[index]
-        detections = self.seq_detection[index]
+        detections = [] if self.seq_detection[index] is None else self.seq_detection[index]
         #TODO: is there a torch function to load images? --> probably
         image = cv2.imread(img_path)
-        batch = {'image': image, 'roi': detections}
+        if self.detection_proposals is not None:
+            proposals = self.detection_proposals[index]
+            proposals_th = [prop for prop in proposals if prop[-1] >= self.prop_TH]
+        else:
+            proposals_th = []
+        batch = {'image': image, 'roi': detections, 'proposals': proposals_th}
         if self.transform is not None:
             batch = self.transform(batch)
         return batch
--- train.py
+++ train.py
@@ -14,6 +14,7 @@ Options:
     -h, --help                  Print this message
     --datapath=<str>            path to the fish dataset sequences [default: data/fishframes/]
     --seqlist=<str>             list of sequences to run [default: data/lists/train.txt]
+    --detpath=<str>             list of sequences to run
     --gpu0=<int>                GPU number [default: 0]
 """
 
@@ -22,9 +23,10 @@ print(args)
 
 seqlist_path = args['--seqlist']
 fishdata_path = args['--datapath']
+detproposal_path = args['--detpath']
 
 num_epochs = 1
-dataset = fdset.FishData(seqlist_path, fishdata_path, 'train')
+dataset = fdset.FishData(seqlist_path, fishdata_path, 'train', det_prop_dir=detproposal_path)
 fish_dloader = DataLoader(dataset)
 
 for epoch_idx in range(num_epochs):
@@ -32,4 +34,4 @@ for epoch_idx in range(num_epochs):
     for frame_idx, fishbatch in enumerate(fish_dloader):
         if frame_idx == 0:
             dtks = fish_dloader.dataset.seq_dets
-            print ('sequence: {} --> {} #detections'.format(fish_dloader.dataset.sequence_key(), len(dtks)))
+            print ('sequence: {} --> {} #detections, proposals: {}'.format(fish_dloader.dataset.sequence_key(), len(dtks), fishbatch['proposals']))
