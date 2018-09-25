72bc078526120e97b27ca54611515a425d0dc98c
Alrik Firl <firla@oregonstate.edu>
2018-09-24 22:15:48 -0700

    WIP

--- convert_MHT.py
+++ convert_MHT.py
@@ -38,8 +38,8 @@ def make_mht_det_infostring(frame, bbox, conf):
     data = [frame, -1, bbox[0], bbox[1], bbox[2], bbox[3], conf, -1, -1, -1]
     return make_mht_infostring(data)
 
-class coco_mapper:
-    def __int__ (self, coco_fpath):
+class CocoMapper:
+    def __init__ (self, coco_fpath):
         self.id_to_img, self.img_to_id = self.load_coco_json(coco_fpath)
 
     def load_coco_json(self, coco_fpath):
@@ -52,20 +52,31 @@ class coco_mapper:
             file_globalid = fdata['id']
             file_path = fdata['file_name']
             fdir_name = os.path.dirname(file_path)
-            if fdir_name not in self.mapper_img_id:
+            fpath_name = os.path.basename(file_path)
+            if fdir_name not in mapper_img_id:
                 mapper_img_id[fdir_name] = {}
 
-            mapper_img_id[fdir_name][file_path] = file_globalid
+            mapper_img_id[fdir_name][fpath_name] = file_globalid
             assert(file_globalid not in mapper_id_img)
             mapper_id_img[file_globalid] = file_path
         return mapper_id_img, mapper_img_id
 
     def get_fpath_for_id(self, gid):
-        return self.coco_mapping[gid]
+        if gid in self.id_to_img:
+            return self.id_to_img[gid]
+        else:
+            return None
+
+    def get_gid_for_path(self, seq, path_id):
+        if path_id in self.img_to_id[seq]:
+            return self.img_to_id[seq][path_id]
+
+        else:
+            return None
 
 def run_MHT_cvt(detfeat_datadir, coco_datainfo, detection_info, mht_dir):
     det_proposal_info = utils.load_detectron_detections(coco_datainfo, detection_info)
-    id_mapper = coco_mapper(coco_datainfo)
+    id_mapper = CocoMapper(coco_datainfo)
     with open(coco_datainfo, 'rt') as f:
         nhfish_data = json.load(f)
 
@@ -73,47 +84,71 @@ def run_MHT_cvt(detfeat_datadir, coco_datainfo, detection_info, mht_dir):
     #NOTE: these are the frames with detection proposals
     dprop_info = det_proposal_info[0]
 
+    fishdata_path = '/home/alrik/Data/NRTFish/val'
+    seqlist_path = 'data/lists/val.txt'
+    #NOTE: this is the data on disk from the labeling, not in coco format
+    nhfish_dset = utils.read_fish_dataset(seqlist_path, fishdata_path)
 
     #for each sequence, grab out the corresponding cnn features and save them out
     for seq_name in id_mapper.img_to_id.keys():
-        mat_structure = {'bx': [], 'by': [], 'x': [], 'y': [], 'w': [],
-                        'h': [], 'r': [], 'fr': [], 'chist': [], 'hog': [], 'cnn': []}
-
         seq_outgt_filepath = os.path.join(mht_dir, seq_name)
         if not os.path.exists(seq_outgt_filepath):
             os.makedirs(seq_outgt_filepath)
+
+        gt_bboxinfo = [fd for fd in nhfish_dset[seq_name] if fd['detections'] is not None]
         outgt_filepath = os.path.join(seq_outgt_filepath, 'gt.txt')
         with open(outgt_filepath, 'wt') as gt_fid:
-            gt_info = nhfish_data['annotations']
-            for gt in gt_info:
-                if gt['image_id'] in
-                gt_str = make_mht_gt_infostring(gt['image_id'], gt['id'], gt['bbox'])
-                gt_fid.write("{}\n".format(gt_str))
+            #gt_info = nhfish_data['annotations']
+            for gt in gt_bboxinfo:
+                frame_fname = os.path.splitext(os.path.basename(gt['frame']))[0]
+                frame_imageid = int(frame_fname)
+                for gt_trkid, gt_bbox in gt['detections'].items():
+                    #change to [upleft x, upleft y, width, height]
+                    gt_bbox_coco = [gt_bbox[0], gt_bbox[1], gt_bbox[2]-gt_bbox[0], gt_bbox[3]-gt_bbox[1]]
+                    gt_str = make_mht_gt_infostring(frame_imageid,  gt_trkid, gt_bbox_coco)
+                    gt_fid.write("{}\n".format(gt_str))
 
-        outdet_filepath = os.path.join(mht_dir, 'det.txt')
+        outdet_filepath = os.path.join(seq_outgt_filepath, 'det.txt')
         with open(outdet_filepath, 'wt') as det_fid:
             for frameid, frameinfo in dprop_info.items():
                 for det_prop in frameinfo['detections']:
                     det_str = make_mht_det_infostring(frameid, det_prop['bbox'], det_prop['score'])
                     det_fid.write("{}\n".format(det_str))
 
+        mat_structure = {'bx': [], 'by': [], 'x': [], 'y': [], 'w': [],
+                            'h': [], 'r': [], 'fr': [], 'chist': [], 'hog': [], 'cnn': []}
 
+        for frame_idx in range(len(nhfish_dset[seq_name])):
+            fpath = nhfish_dset[seq_name][frame_idx]['frame']
+            fdir, fname = fpath.split(os.sep)[-2:]
+            assert(fdir == seq_name)
+            gid = id_mapper.get_gid_for_path(seq_name, fname)
 
+            #NOTE: if gid == None, then the current frame has no annotations, and we skip it
+            if gid is None:
+                continue
 
-
-        for frameid, frameinfo in dprop_info.items():
-
-            feat_filepath = os.path.join(detfeat_datadir, str(frameid) + '.npz')
-            if os.path.exists(feat_filepath):
-                cnn_framefeat = np.load(feat_filepath)['data'][None][0]
-            else:
+            fnum = int(os.path.splitext(fname)[0])
+            feat_fname = str(fnum) + '.npz'
+            feat_fname_path = os.path.join(detfeat_datadir, feat_fname)
+            if not os.path.exists(feat_fname_path):
+                #NOTE: this means that there was a gt bbox, but there was no detection for it (and thus no cnn feature)
+                #raise RuntimeError("cnn feature file {} must exist".format(feat_fname_path))
                 cnn_framefeat = None
+            else:
+                cnn_framefeat = np.load(feat_fname_path)['data'][None][0]
 
-            for det_idx, det_prop in enumerate(frameinfo['detections']):
+            #TODO: we basically want to loop over all the annotation text files in the
+            #sequence directory (e.g. at /home/alrik/Data/NRTFish/train), and for each
+            #file, get the global id for the coco formatting (i.e. using the
+            #id_mapper's img --> id), then grab the cnn feature file for that global id,
+            #then save that feature out in the mat structure.
+            #We also want to grab the bbox data from each file, and the sequence-specific
+            #image id
 
+            for det_idx, det_prop in enumerate(dprop_info[gid]['detections']):
                 #TODO: need to append to the structure for each field
-                mat_structure['fr'].append(frameid)
-
+                mat_structure['fr'].append(frame_idx)
                 mat_structure['bx'].append(det_prop['bbox'][0])
                 mat_structure['x'].append(det_prop['bbox'][0])
                 mat_structure['by'].append(det_prop['bbox'][1])
@@ -151,7 +186,6 @@ def large_mht(detfeat_datadir, coco_datainfo, detection_info, mht_dir):
     with open(outgt_filepath, 'wt') as gt_fid:
         gt_info = nhfish_data['annotations']
         for gt in gt_info:
-            if gt['image_id'] in
             gt_str = make_mht_gt_infostring(gt['image_id'], gt['id'], gt['bbox'])
             gt_fid.write("{}\n".format(gt_str))
 
